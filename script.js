022db62322d2e03364ba1a19ba54640fef0e3ed8
Vincent Kempers <me@vncn.tk>
2018-10-27 17:45:44 +0200

    vanilla fullpage edit and change in images

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
       let htmlValues = Object.values(allItems);
       htmlValues.forEach(i => {
         i.addEventListener("mouseover", event => {
-          imageSrc.src = `imgs/${event.target.classList[0]}.png`;
+          imageSrc.src = `imgs/${event.target.classList[0]}.jpg`;
           console.log(event.target.classList[0]);
         });
       });
