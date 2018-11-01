8ee1dcca961cd983e00fb5797883d01d9e0c7391
Vincent Kempers <me@vncn.tk>
2018-11-01 12:24:05 +0100

    add chartjs code

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
   let app = {
     init() {
       changeSRC.init();
-    },
-    googleChart() {
-      googleChartSetup.init();
+      timeChart.init();
     }
   };
 
@@ -18,6 +16,56 @@
           imageSrc.src = `imgs/${event.target.classList[0]}.jpg`;
           console.log(event.target.classList[0]);
         });
+      });
+    }
+  };
+
+  let timeChart = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    data: [12, 19, 3, 5, 2, 3],
+    bgColor: [
+      "rgba(255, 99, 132, 0.2)",
+      "rgba(54, 162, 235, 0.2)",
+      "rgba(255, 206, 86, 0.2)",
+      "rgba(75, 192, 192, 0.2)",
+      "rgba(153, 102, 255, 0.2)",
+      "rgba(255, 159, 64, 0.2)"
+    ],
+    borderColor: [
+      "rgba(255,99,132,1)",
+      "rgba(54, 162, 235, 1)",
+      "rgba(255, 206, 86, 1)",
+      "rgba(75, 192, 192, 1)",
+      "rgba(153, 102, 255, 1)",
+      "rgba(255, 159, 64, 1)"
+    ],
+    ctx: document.getElementById("timeChart").getContext("2d"),
+    init() {
+      var myChart = new myChart(ctx, {
+        type: "bar",
+        data: {
+          labels: this.labels,
+          datasets: [
+            {
+              label: "# of Votes",
+              data: this.data,
+              backgroundColor: this.bgColor,
+              borderColor: this.borderColor,
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            ]
+          }
+        }
       });
     }
   };
