e16000cc5065e0dcc4c4e35fda586c976a84efab
Vincent Kempers <me@vncn.tk>
2018-11-13 13:57:05 +0100

    add new logic for questionnaire

--- questionnaire.js
+++ questionnaire.js
@@ -1,10 +1,26 @@
 let questionnaire = (function() {
   return {
+    chosenAnswers: [],
+    checkArray(theWord) {
+      this.chosenAnswers.push(theWord);
+    },
     init() {
-      let allAnwsers = document.querySelector(".answers").children;
-      let htmlValues = Object.values(allAnwsers);
-      console.log(htmlValues);
-      let chosenAnswers = [];
+      let allAnwsers = document.querySelector(".answers");
+      allAnwsers.addEventListener(
+        "click",
+        function(event) {
+          console.log(event);
+          if (event.target.nodeName == "LABEL") {
+            event.target.classList.toggle("rick");
+            console.log("true");
+            questionnaire.checkArray(event.target.innerText);
+          } else {
+            console.log("it's not working");
+          }
+          console.log(questionnaire.chosenAnswers);
+        },
+        true
+      );
     }
   };
 })();
