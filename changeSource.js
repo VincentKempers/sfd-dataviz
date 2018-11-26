1cd6a5c4050beab0abbb8d695c89f15c54ca58fd
Vincent Kempers <me@vncn.tk>
2018-11-26 18:58:49 +0100

    change behaviour of hover and add new style

--- changeSource.js
+++ changeSource.js
@@ -1,62 +1,65 @@
 (function() {
-  let jamesVP = {
-    name: "James van Praagh",
-    age: "50",
-    books: [
-      "Ghosts Among Us",
-      "Reaching to Heaven",
-      "Healing grief",
-      "Looking Beyond",
-      "Talking to heaven",
-      "Heaven and Earth",
-      "Meditations with James Van Praagh",
-      "Tuning Into: Intuition/Abundance"
-    ],
-    tv: [],
-    networth: "200.6 miljoen"
-  };
-  johnE = {
-    name: " John Edward",
-    age: "",
-    books: [
-      "infinite quest",
-      "After Life",
-      "One Last time",
-      "Fallen Masters",
-      "Crossing Over",
-      "Practical Praying",
-      "Final Beginnings",
-      "Developing Your own Psychic Powers",
-      "What If God Were the SUN"
-    ],
-    tv: [
-      "Crossing Over with John Edward",
-      "John Edward Cross Country",
-      "EVOLVE (internet show)"
-    ],
-    networth: "3 miljoen"
-  };
-
-  let missC = {
-    name: " Miss Cleo",
-    age: "overleden",
-    books: [
-      "infinite quest",
-      "After Life",
-      "One Last time",
-      "Fallen Masters",
-      "Crossing Over",
-      "Practical Praying",
-      "Final Beginnings",
-      "Developing Your own Psychic Powers",
-      "What If God Were the SUN"
-    ],
-    tv: [
-      "Crossing Over with John Edward",
-      "John Edward Cross Country",
-      "EVOLVE (internet show)"
-    ],
-    networth: "3 miljoen"
+  let medium = {
+    jamesVP: {
+      name: "James van Praagh",
+      age: "60",
+      books: [
+        "Ghosts Among Us",
+        "Reaching to Heaven",
+        "Healing grief",
+        "Looking Beyond",
+        "Talking to heaven",
+        "Heaven and Earth",
+        "Meditations with James Van Praagh",
+        "Tuning Into: Intuition/Abundance"
+      ],
+      tv: [],
+      networth: "200.6 miljoen",
+      audio: ""
+    },
+    johnE: {
+      name: " John Edward",
+      age: "49",
+      books: [
+        "infinite quest",
+        "After Life",
+        "One Last time",
+        "Fallen Masters",
+        "Crossing Over",
+        "Practical Praying",
+        "Final Beginnings",
+        "Developing Your own Psychic Powers",
+        "What If God Were the SUN"
+      ],
+      tv: [
+        "Crossing Over with John Edward",
+        "John Edward Cross Country",
+        "EVOLVE (internet show)"
+      ],
+      networth: "3 miljoen",
+      audio: ""
+    },
+    missC: {
+      name: " Miss Cleo",
+      age: "overleden",
+      books: ["Miss cleo Tarot power deck"],
+      tv: [
+        "Miss Cleo",
+        "Miss Cleo psychic advise",
+        "Miss Cleo tarot power",
+        "Mind and spirit"
+      ],
+      networth: "1 miljoen",
+      audio: ""
+    },
+    hollywoodM: {
+      name: "Tyler Henry",
+      age: "22",
+      books: ["Between Two Worlds: Lessons from the other side"],
+      tv: ["Hollywood medium"],
+      networth: "3 miljoen",
+      audio: ""
+    }
   };
 
   let changeSRC = {
@@ -66,16 +69,17 @@
       let htmlValues = Object.values(allItems);
       htmlValues.forEach(i => {
         i.addEventListener("mouseover", event => {
-          infoPlace.innerText = `
+          console.log(event.target.classList[0]);
+          let targetName = event.target.classList[0];
+          let rightMedium = medium[`${targetName}`];
+
+          infoPlace.innerHTML = `
           <ul>
-          <li>${medium.name}</li>
-          <li>${medium.age}</li>
-          <li>${medium.books}</li>
-          <li>${medium.networth}</li>
+            <li> ${rightMedium.name}</li>
+            <li>Netto waarde: ${rightMedium.networth}</li>
           </ul>
           
           `;
-          console.log(event.target.classList[0]);
         });
       });
     }
